8885c669c8738c02cd0e4335a8c9f2c58707378c
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-01-25 06:45:41 -0500

    Setup the inquirer prompt list of questions

--- index.js
+++ index.js
@@ -1,8 +1,71 @@
-// TODO: Include packages needed for this application
+// Declaring dependencies
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme");
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this projects?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue? "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+    
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
